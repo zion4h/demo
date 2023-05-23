b9661b9b5678656804087ebbf2d65f91386a06a6
zion4h <91511906+zion4h@users.noreply.github.com>
2023-05-23 17:56:09 +0800

    feat: init

---
+++ animal.py
@@ -0,0 +1,20 @@
+import sys
+
+def cat():
+    print('Meow!')
+def dog():
+    print('Woof!')
+
+def default():
+    print('hello')
+
+def main():
+    if sys.argv[1] == 'cat':
+        cat()
+    elif sys.argv[1] == 'dog':
+        dog()
+    else:
+        default()
+
+if __name__ == '__main__':
+    main()
---
+++ script.py
@@ -0,0 +1,5 @@
+def double(x):
+   breakpoint()
+   return x * 2
+val = 3
+print(f"{val} * 2 is {double(val)}")
